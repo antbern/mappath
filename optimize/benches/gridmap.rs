8b0eec40a824150a22794fe90574afab2d9cd9c8
antbern <40672068+antbern@users.noreply.github.com>
2024-03-03 21:34:38 +0100

    improve benchmark using groups and `bench_with_input`

--- optimize/benches/gridmap.rs
+++ optimize/benches/gridmap.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use optimize::{util::parse_img, GridMap, MapTrait, PathFinder, PathFinderState, Point};
 
 fn load_base_map_scaled(factor: usize) -> (GridMap<usize>, Point, Point) {
@@ -16,33 +16,24 @@ fn load_base_map_scaled(factor: usize) -> (GridMap<usize>, Point, Point) {
     (map, start, goal)
 }
 
-fn bench_map_scaled(c: &mut Criterion, factor: usize) {
-    let (map, start, goal) = load_base_map_scaled(factor);
-
-    c.bench_function(&format!("map_scaled_{}", factor), |b| {
-        b.iter(|| {
-            let (res, _) = PathFinder::new(
-                black_box(start),
-                black_box(goal),
-                black_box(map.create_storage()),
-            )
-            .finish(&map);
-            assert!(matches!(res, PathFinderState::PathFound(_)));
-        })
-    });
+pub fn map_scaled_factor(c: &mut Criterion) {
+    let mut group = c.benchmark_group("map_scaled_factor");
+    for factor in [1, 2, 3, 4, 5, 6, 7, 8].iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(factor), factor, |b, &factor| {
+            let (map, start, goal) = load_base_map_scaled(factor);
+            b.iter(|| {
+                let (res, _) = PathFinder::new(
+                    black_box(start),
+                    black_box(goal),
+                    black_box(map.create_storage()),
+                )
+                .finish(&map);
+                assert!(matches!(res, PathFinderState::PathFound(_)));
+            });
+        });
+    }
+    group.finish();
 }
 
-pub fn map_small(c: &mut Criterion) {
-    bench_map_scaled(c, 1);
-}
-
-pub fn map_medium(c: &mut Criterion) {
-    bench_map_scaled(c, 2);
-}
-
-pub fn map_large(c: &mut Criterion) {
-    bench_map_scaled(c, 4);
-}
-
-criterion_group!(benches, map_small, map_medium, map_large);
+criterion_group!(benches, map_scaled_factor);
 criterion_main!(benches);
