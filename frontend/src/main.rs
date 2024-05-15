6f550d54c2da1ee751fd4c95d3de3446f2cf3b69
antbern <40672068+antbern@users.noreply.github.com>
2024-05-15 16:23:56 +0200

    rename App

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(frontend::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(frontend::App::new(cc))),
     )
 }
 
@@ -37,9 +37,10 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(frontend::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(frontend::App::new(cc))),
             )
             .await
             .expect("failed to start eframe");
     });
 }
+
